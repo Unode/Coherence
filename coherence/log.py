d0baefb423965591b8887a5772e4c9f58b6eef09
Tomasz Lakota <tomasz.lakota@gmail.com>
2013-11-02 23:14:27 +0100

    overriding "findCaller" of logging.Logger so the file:line in log records are correct

--- coherence/log.py
+++ coherence/log.py
@@ -5,14 +5,58 @@
 
 import os
 import logging
-from logging import *
+import sys
 
-LOG_FORMAT =('%(levelname)s %-27(category)s '
+LOG_FORMAT =('%(levelname)s %-27(name)s '
              '%(asctime)s %(message)s '
              '(%(filename)s:%(lineno))')
 
 ENV_VAR_NAME = 'COHERENCE_DEBUG'
 
+#copy-pasted from "logging"
+if hasattr(sys, 'frozen'): #support for py2exe
+    _srcfile = "logging%s__init__%s" % (os.sep, __file__[-4:])
+elif __file__[-4:].lower() in ['.pyc', '.pyo']:
+    _srcfile = __file__[:-4] + '.py'
+else:
+    _srcfile = __file__
+_srcfile = os.path.normcase(_srcfile)
+
+#copy-pasted from "logging"
+def currentframe():
+    import sys
+    """Return the frame object for the caller's stack frame."""
+    try:
+        raise Exception
+    except:
+        return sys.exc_info()[2].tb_frame.f_back
+
+class Logger(logging.Logger):
+    def __init__(self, name, level=logging.NOTSET):
+        logging.Logger.__init__(self, name, level)
+        pass
+
+#copy-pasted from "logging"
+    def findCaller(self):
+        """
+        Find the stack frame of the caller so that we can note the source
+        file name, line number and function name.
+        """
+        f = currentframe()
+        #On some versions of IronPython, currentframe() returns None if
+        #IronPython isn't run with -X:Frames.
+        if f is not None:
+            f = f.f_back
+        rv = "(unknown file)", 0, "(unknown function)"
+        while hasattr(f, "f_code"):
+            co = f.f_code
+            filename = os.path.normcase(co.co_filename)
+            if filename == logging._srcfile or filename == _srcfile:
+                f = f.f_back
+                continue
+            rv = (co.co_filename, f.f_lineno, co.co_name)
+            break
+        return rv
 
 class Loggable(logging.Logger):
     """
@@ -25,6 +69,8 @@ class Loggable(logging.Logger):
     """
 
     logCategory = 'default'
+    def __init__(self):
+        pass
 
     def logObjectName(self):
         """Overridable object name function."""
@@ -64,6 +110,8 @@ class Loggable(logging.Logger):
 def init(logfilename=None, loglevel=logging.WARN):
     logger = logging.getLogger()
     logging.addLevelName(100, 'NONE')
+
+    logging.Logger.manager.setLoggerClass(Logger)
 
     logging.basicConfig(filename=logfilename, level=loglevel,
                         format=LOG_FORMAT)
