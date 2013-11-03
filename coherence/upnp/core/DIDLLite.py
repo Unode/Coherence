a103974b8f32e2bcc31c151127dc0637e730eee9
Tomasz Lakota <tomasz.lakota@gmail.com>
2013-11-03 13:40:00 +0100

    multiple xmlns:dc and xmlns:upnp attributes fixed

--- coherence/upnp/core/DIDLLite.py
+++ coherence/upnp/core/DIDLLite.py
@@ -926,8 +926,7 @@ class DIDLElement(ElementInterface,log.Loggable):
                  transcoding=False):
         ElementInterface.__init__(self, 'DIDL-Lite', {})
         self.attrib['xmlns'] = 'urn:schemas-upnp-org:metadata-1-0/DIDL-Lite/'
-        self.attrib['xmlns:dc'] = 'http://purl.org/dc/elements/1.1/'
-        self.attrib['xmlns:upnp'] = 'urn:schemas-upnp-org:metadata-1-0/upnp/'
+        #xmlns:dc and xmlsn:upnp are intentionally omitted here, since they're already added automagically via namespaces
         self.attrib['xmlns:dlna'] = 'urn:schemas-dlna-org:metadata-1-0'
         self.attrib['xmlns:pv'] = 'http://www.pv.com/pvns/'
         self._items = []
